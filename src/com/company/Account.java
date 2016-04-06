de109a2bdc913da5c4c0cf260479798b6f909a00
Sajedeh Panahi <sajedehpanahi@gmail.com>
2016-04-06 15:44:27 +0430

    after adding setter and getter

--- src/com/company/Account.java
+++ src/com/company/Account.java
@@ -7,11 +7,97 @@ import java.math.BigDecimal;
 public abstract class Account {
 
     private int customerNumber;
-    protected int durationDays;
-    protected BigDecimal depositBalance;
-    protected BigDecimal profit;
+    private int durationDays;
+    private BigDecimal depositBalance;
+    private BigDecimal profit=new BigDecimal("0");
     private String depositType;
-    protected String errorMessage;
+    private String errorMessage=null;
+
+    public int getCustomerNumber(){
+        return customerNumber;
+    }
+
+    public void setCustomerNumber(int number){
+        customerNumber=number;
+    }
+
+    public int getDurationDays(){
+        return durationDays;
+    }
+
+    public void setDurationDays(int days){
+         int flag=0;
+        try{
+            if(days>0)
+                durationDays = days;
+            else if(days == 0){
+                flag=1;
+                throw new Exception() ;
+            }else if(days < 0){
+                flag=2;
+                throw new Exception() ;
+            }
+        }catch(Exception e){
+            if(flag==1)
+                errorMessage="Duration Days Cannot Be 0!";
+            else if(flag == 2)
+                errorMessage="Duration Days Cannot Be Negative!";
+        }
+    }
+
+    public BigDecimal getDepositBalance(){
+        return depositBalance;
+    }
+
+    public void setDepositBalance(BigDecimal balance){
+        int flag=0;
+        int result = balance.compareTo(new BigDecimal("0"));
+        try{
+            if(result == 0 || result == 1 )
+                depositBalance = balance;
+            else if(result == -1) {
+                flag = 1;
+                throw new Exception() ;
+            }
+        }catch(Exception e){
+            if(flag==1)
+                errorMessage="Deposit Balance Cannot Be Negative!";
+        }
+    }
+
+    public BigDecimal getProfit(){
+
+        return profit;
+    }
+
+    public void setProfit(BigDecimal prf){
+        profit = prf;
+    }
+
+    public String getDepositType(){
+        return depositType;
+    }
+
+    public void setDepositType(String type){
+        type = type.toLowerCase();
+        try {
+            if (type == "qarz" || type == "shortterm" || type == "longterm")
+                depositType = type;
+            else {
+                throw new Exception() ;
+            }
+        }catch (Exception e){
+            errorMessage = "Unknown Deopsit Type!";
+        }
+    }
+
+    public String getErrorMessage(){
+        return errorMessage;
+    }
+
+   /* private void setErrorMessage(String msg){
+        errorMessage = msg;
+    }*/
 
     public Account(){
     }
@@ -24,6 +110,7 @@ public abstract class Account {
     }
 
     public BigDecimal calulateDeposit(){
+        //do something if errormessage != null
 
         BigDecimal iR = getInterestRate();
         BigDecimal dD = new BigDecimal(durationDays);
